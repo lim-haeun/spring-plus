978dea8949570340e78b23a67d81bc231155f7aa
임하은 <gkdmsdl100414@gmail.com>
2026-04-06 00:54:55 +0900

    refactor: Todo 단건 조회를 QueryDSL로 변경

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -10,26 +10,21 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoRepositoryCustom {
 
     @Query("""
-    SELECT t
-    FROM Todo t
-    LEFT JOIN FETCH t.user
-    WHERE (:weather IS NULL OR t.weather = :weather)
-      AND (:startDate IS NULL OR t.modifiedAt >= :startDate)
-      AND (:endDate IS NULL OR t.modifiedAt <= :endDate)
-    ORDER BY t.modifiedAt DESC
-""")
+                SELECT t
+                FROM Todo t
+                LEFT JOIN FETCH t.user
+                WHERE (:weather IS NULL OR t.weather = :weather)
+                  AND (:startDate IS NULL OR t.modifiedAt >= :startDate)
+                  AND (:endDate IS NULL OR t.modifiedAt <= :endDate)
+                ORDER BY t.modifiedAt DESC
+            """)
     Page<Todo> searchTodos(
             @Param("weather") String weather,
             @Param("startDate") LocalDateTime startDate,
             @Param("endDate") LocalDateTime endDate,
             Pageable pageable
     );
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
